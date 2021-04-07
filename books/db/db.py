cff255c080ab1b650f81a0cab8d807fc6a4c1ac8
Natnael Abay <se.natnael.abay@gmail.com>
2021-04-07 23:07:23 +0300

    [db] db setup localy

---
+++ books/db/db.py
@@ -0,0 +1,41 @@
+import os
+import click
+from sqlalchemy import create_engine,text
+from sqlalchemy.orm import scoped_Session,sessionmaker
+from flask import current_app,g
+from flask.cli import with_appcontext
+from dotenv import load_dotenv
+load_dotenv()
+
+
+def get_db():
+    if 'db' not in g:
+        engine = create_engine(os.getenv("DATABASE_URL"))
+        db = scoped_Session(sessionmaker(bind=engine))
+        g.db = db
+    return g.db
+
+def close_db(e=None):
+    db = g.pop('db')
+    if db is not None:
+        db.close()
+
+
+
+def init_db():
+    db = get_db()
+    with current_app.open_resource(os.path.join("db", "schema.sql")) as f:
+        query = text(f.read().decode('utf8'))
+        db.execute(query)
+        db.commit()
+
+@click.commad("build-db")
+@with_appcontext
+def execute_command():
+    init_db()
+    click.echo("database schema built!")
+
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
+    
