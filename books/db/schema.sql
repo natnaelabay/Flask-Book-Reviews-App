5920b34007bc4e02eff151f0084c7479a356455e
Natnael Abay <se.natnael.abay@gmail.com>
2021-04-07 23:08:41 +0300

    [db] create a schema file for defining tables

---
+++ books/db/schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE USERS (
+    id    int NOT NULL,
+    f_name VARCHAR(30),
+    l_name VARCHAR(30),
+    u_name VARCHAR(15),
+    password VARCHAR(255),
+    profile_url VARCHAR(255)
+    PRIMARY KEY(id) 
+)
