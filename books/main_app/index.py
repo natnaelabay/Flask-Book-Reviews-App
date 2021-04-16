1caf62ae6291ac6f00bc4d0b72d2514bf1c952ae
Natnael Abay <se.natnael.abay@gmail.com>
2021-04-16 23:17:11 +0300

    [main] search endpoint args changed

--- books/main_app/index.py
+++ books/main_app/index.py
@@ -1,48 +1,49 @@
 from flask import (
-	Blueprint, flash, g, redirect, render_template, request, session, url_for,json
+	Blueprint, flash, g, redirect, render_template, request, session, url_for,json,jsonify
 )
 from werkzeug.security import check_password_hash , generate_password_hash
 from books.db.db import get_db
 from books.utlis.utils import build_sql
-from books.auth.auth import login_required
+from books.auth.auth import login_required, redirect_if_logged_in
+from sqlalchemy import text
 bp = Blueprint("main", __name__)
-
+ 
 @bp.route("/home/")
 @bp.route("/")
+@redirect_if_logged_in
 def index():
     return render_template("home.html")
 
 
-@bp.route('/return_res' , methods=["GET"])
-def return_res():
+@bp.route('/search' , methods=["GET"])
+def search_book():
     db = get_db()
-    isbn = request.args.get('isbn')
-    title  = request.args.get('title')
-    author = request.args.get('author')
-    books = [title , isbn , author]
-    commands = []
-    for i in range(len(books)):
-        if books[i] is not None and i == 0:
-            commands.append(build_sql(books[i],'title'))
-        if books[i] is not None and i == 1:
-            commands.append(build_sql(books[i],'isbn'))
-        if books[i] is not None and i == 2:
-            commands.append(build_sql(books[i],'author'))
-    sql_query = ""
-    for i in  range(len(commands)):
-        if i == 0:
-             sql_query = commands[0]+ "  "
-        else:
-            sql_query += "and " +  commands[i] + "  " 
-    obj_books = db.execute(
-        "select * from books where " + sql_query  + "  order by year desc limit 10;"
-        ).fetchall() 
+    payload = request.args.get("payload")
+    search_category = request.args.get("search_category")
+    if payload is None or search_category is None:
+        error = {"success" : False, "message" : "Some of the inputs are empty! "}
+        return jsonify(error)
+    obj_books = []
+    if search_category == "year":
+        obj_books = db.execute(
+            f"select * from books where {search_category} = '%{int(payload)}%' limit 200 ;" , {
+                "payload" : payload,
+                "search_category" : search_category
+            }
+            ).fetchall()
+    else:
+        obj_books = db.execute(
+            f"select * from books where {search_category} LIKE '%{payload}%' limit 200 ;" , {
+                "payload" : payload,
+                "search_category" : search_category
+            }
+            ).fetchall() 
     data = []
     for row in obj_books:
         r = [x for x in row]
         d = { "pub_date" : r[0], "title" : r[2], "isbn" : r[1].strip(), "author" :r[3] }
         data.append(d)
-    return json.dumps(data)
+    return jsonify({"data" : data , "success" : True})
 
 
 
@@ -55,5 +56,10 @@ def book(isbn):
 
 @bp.route("/profile")
 @login_required
-def reviews():
-    return "there was an error"
+def profile():
+    return render_template("profile.html")
+
+
+# @bp.route("/a")
+# def profile():
+#     return render_template("profile.html")
